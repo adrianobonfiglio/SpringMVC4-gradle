5bc7134921c8d6bd4505c8734820eb445b3d47e8
adrianobonfiglio <adrianobonfiglio@gmail.com>
2014-11-21 00:48:04 -0200

    Configurações do hibernate

--- src/main/java/br/com/gradletest/repository/PessoaRepositoryIntf.java
+++ src/main/java/br/com/gradletest/repository/PessoaRepositoryIntf.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gradletest.entity.Pessoa;
 
--- src/main/java/br/com/gradletest/service/PessoaService.java
+++ src/main/java/br/com/gradletest/service/PessoaService.java
@@ -1,8 +1,13 @@
 package br.com.gradletest.service;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import br.com.gradletest.entity.Pessoa;
 import br.com.gradletest.repository.PessoaRepositoryIntf;
@@ -11,9 +16,10 @@ import br.com.gradletest.repository.PessoaRepositoryIntf;
  * @author Adriano S. Bonfiglio
  *
  */
-public class PessoaService {
+@Service
+public class PessoaService implements PessoaRepositoryIntf {
 	
-	@Autowired
+	@Resource
 	PessoaRepositoryIntf pessoaRepository;
 	
 	/**
@@ -30,6 +36,66 @@ public class PessoaService {
 	 */
 	public List<Pessoa> findAll() {
 		return pessoaRepository.findAll();
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(Serializable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Pessoa arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Iterable<? extends Pessoa> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean exists(Serializable arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<Pessoa> findAll(Iterable<Serializable> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa findOne(Serializable arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Pessoa> S save(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Pessoa> Iterable<S> save(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
  
 }
